60b0242c70c3789284d8f3805e1a07d55d90899e
fractaltruth <satyad18@gmail.com>
2022-04-17 14:41:36 +0200

    C Committing the code for pushin

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+        matrix_inverse <- NULL
+        set <- function(y){
+                x <<- y
+                matrix_inverse <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) matrix_inverse <<- inverse
+        getinverse <- function() matrix_inverse
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        matrix_inverse <- x$getinverse()
+        if(!is.null(matrix_inverse)) {
+                message("getting inverse matrix from cache")
+                return(matrix_inverse)
+        }
+        data <- x$get()
+        matrix_inverse <- solve(data, ...)
+        x$setinverse(matrix_inverse)
+        matrix_inverse
 }
